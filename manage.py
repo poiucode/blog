d8ba6e6f8139bc9929401bdd04a9a7236668490c
jon <poiucode@gmail.com>
2022-08-19 15:58:27 -0400

    can't get navbar to work

---
+++ manage.py
@@ -0,0 +1,4 @@
+import utils.py
+
+if __name__ == "__main__":
+    # run function from utils.py
--- utils.py
+++ utils.py
@@ -21,31 +21,40 @@ pages = [
     {
         'template'  : 'index',
         'title': 'Home',
+        'url': 'index.html',
         'heading': 'Say hello to my little blog!',
         'content': "Welcome to a small templated blog",
     },
     {
         'template'  : 'blog',
         'title': 'Blog',
+        'url': 'blog.html',
         'heading': 'Blog',
         'content': "", # pull as list of items from separate file    
     },
     {
         'template'  : 'contact',
         'title': 'Contact',
+        'url': 'contact.html',
         'heading': 'Say hello!',
         'content': "Submit a pr and I'll get back to you",    
-    },
+    },  
 ]
 
-# cycle through the pages list                            
+# build navbar
+navbar = []
+for page in pages: 
+    navbar.append({
+        'link': page['url'],
+        'link_title': page['title']
+    })
+    
+# cycle through the pages list to build the output files                         
 for page in pages:
     # use get_template(), which accesses /templates, and pulls in the template file using 'template' name within the current dict 
     template = env.get_template(f"{page['template']}.html")
     # render() takes in either a dict or a string. In this case, we pass in the dict and it does the work for us
-    context = template.render(
-        page        
-    )
+    context = template.render(page, navbar=navbar)
     # Having just accessed our jinja template, we now prepare the output files in docs_filename variables                              
     if page['template'] == 'index':
         docs_filename = "./docs/index.html"
@@ -56,3 +65,6 @@ for page in pages:
     with open(docs_filename, mode="w", encoding="utf-8") as output:
         output.write(context)
     # print(f"... wrote {filename}")
+
+                                
+print("files created")
